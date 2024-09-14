d6dcfcf8a244373b84191b799d0040b195e74836
Utkarsh Kadam <kadamutkarsh21@gmail.com>
2024-09-14 16:06:14 +0530

    fixes

--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -20,7 +20,7 @@ async function dbConnect(): Promise<void> {
     const db = await mongoose.connect(process.env.MONGODB_URI || "", {
       dbName: "fundrr",
     });
-    console.log(db);
+    //console.log(db);
 
     connection.isConnected = db.connections[0].readyState;
     console.log("Database is connected successfully");
