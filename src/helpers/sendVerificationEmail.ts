3ca1b2424d5d8002ba7c47f9fad184741f9bcc30
Utkarsh Kadam <kadamutkarsh21@gmail.com>
2024-09-14 16:07:58 +0530

    change the email template

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -1,22 +1,31 @@
 import { resend } from "@/lib/resend";
-import VerificationEmail from "@/components/VerificationEmail";
+import VerificationEmail from "../../emails/VerificationEmail";
 import { ApiResponse } from "@/types/ApiResponse";
+import { ApiErrorResponse } from "@/types/ApiErrorResponse";
 
 export async function sendVerificationEmail(
   email: string,
   username: string,
   verifyCode: string,
-): Promise<ApiResponse> {
+): Promise<ApiResponse | ApiErrorResponse> {
   try {
     await resend.emails.send({
-      from: "onboarding@fundrr",
+      from: "onboarding@resend.dev",
       to: email,
       subject: "fundrr | verification code",
       react: VerificationEmail({ username, otp: verifyCode }),
     });
-    return { success: true, message: "Verification email sent successfully" };
+    return {
+      success: true,
+      message: "Verification email sent successfully",
+      status: 201,
+    };
   } catch (emailError) {
     console.error("Error in sending verification email", emailError);
-    return { success: false, message: "Failed to send verification email" };
+    return {
+      success: false,
+      message: "Failed to send verification email",
+      errorCode: 500,
+    };
   }
 }
