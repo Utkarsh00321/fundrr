11fe40a5b3a3cf7e350c46194923e531a5562740
Utkarsh Kadam <kadamutkarsh21@gmail.com>
2024-09-24 16:36:31 +0530

    update next-auth types

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,41 +1,42 @@
 import "next-auth"; // Importing the next-auth library for type augmentation
+import { DefaultSession } from "next-auth"; // Importing DefaultSession for augmentation
 
 // Augmenting the 'next-auth' module to extend the User and Session interfaces
-declare module 'next-auth' {
-    // Extending the User interface to include custom properties
-    interface User {
-        _id:string;
-        username:string;
-        email:string;
-        isVerified:boolean;
-        profilePicture:string;
-        bio?:string;
-    }
+declare module "next-auth" {
+  // Extending the User interface to include custom properties
+  interface User {
+    _id: string;
+    username: string;
+    email: string;
+    isVerified: boolean;
+    profilePicture: string;
+    bio?: string;
+  }
 
-    // Extending the Session interface to include custom properties in the user object
-    interface Session {
-        user: {
-            _id:string;
-            username:string;
-            email:string;
-            isVerified:boolean;
-            profilePicture:string;
-            bio?:string;
-        } & DefaultSession['user']; // Including the default properties from DefaultSession's user
-    }
+  // Extending the Session interface to include custom properties in the user object
+  interface Session {
+    user: {
+      _id: string;
+      username: string;
+      email: string;
+      isVerified: boolean;
+      profilePicture: string;
+      bio?: string;
+    } & DefaultSession["user"]; // Including the default properties from DefaultSession's user
+  }
 }
 
 // Augmenting the 'next-auth/jwt' module to extend the JWT interface
-declare module 'next-auth/jwt' {
-    // Extending the JWT interface to include custom properties in the user object
-    interface JWT {
-        user: {
-            _id:string;
-            username:string;
-            email:string;
-            isVerified:boolean;
-            profilePicture:string;
-            bio?:string;
-        }
-    }
+declare module "next-auth/jwt" {
+  // Extending the JWT interface to include custom properties in the user object
+  interface JWT {
+    user: {
+      _id: string;
+      username: string;
+      email: string;
+      isVerified: boolean;
+      profilePicture: string;
+      bio?: string;
+    };
+  }
 }
